065ed3e6ad22439b9bd6743f89f1c74595b8b689
Michael Conry <mikeconry@gmail.com>
2019-04-16 15:09:58 -0400

    all

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import logo from './logo/svg';
+import logo from './logo/svg';
 import './App.css';
 import Menu from './components/Menu';
 import Search from './components/Search';
@@ -225,7 +225,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">NYT Google Books App!</h1>
         </header>
 
